89816e63d0fab9d20a24d8ba12e15381d5618d4c
jack-michaud <mrmichaud42@gmail.com>
2018-05-08 21:14:29 -0400

    commit server boilerplate and Script abstract class

---
+++ web/Script/AbstractScript.py
@@ -0,0 +1,14 @@
+
+class AbstractScript(object):
+
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def is_done(self):
+        pass
+
+    def finish(self):
+        pass
--- web/server.py
+++ web/server.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', message="Hey!")
 
-@app.route("/update", method="POST")
+@app.route("/update", methods=["POST"])
 def update_script():
+    pass
+
+@app.route("/upload", methods=["POST"])
+def upload_new_script():
+    pass
+
+@app.route("/run", methods=["POST"])
+def run_script():
     pass
 
 
