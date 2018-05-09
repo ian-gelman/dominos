d99fe15983dd0d3ef568d6d5415bb06a39fccc37
jack-michaud <mrmichaud42@gmail.com>
2018-05-08 21:15:52 -0400

    abc for python

--- web/Script/AbstractScript.py
+++ web/Script/AbstractScript.py
@@ -1,14 +1,18 @@
+from abc import abstractmethod
 
 class AbstractScript(object):
 
     def __init__(self):
         pass
 
+    @abstractmethod
     def start(self):
         pass
 
+    @abstractmethod
     def is_done(self):
         pass
 
+    @abstractmethod
     def finish(self):
         pass
